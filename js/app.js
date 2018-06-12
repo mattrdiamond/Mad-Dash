60b79495809c7f494c98b202863eb8c1ed8721ae
Matt Diamond <mattdiamond80@gmail.com>
2018-06-11 19:48:21 -0500

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -97,7 +97,6 @@ for (var i = 0; i < 3; i++) {
 }
 
 
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
